4989148176a82831a6a9a0192cc435f98a5c5949
Per Lundgren <per.lundgren@vinnovera.se>
2015-12-10 17:40:26 +0100

    js clean
    Signed-off-by: Per Lundgren <per.lundgren@vinnovera.se>

--- src/js/main.js
+++ src/js/main.js
@@ -1,35 +1,6 @@
 !function(w, d, $) {
 
-	var
-		iAmSuper    	= false,
-		iAmDouble	 	= false,
-		tabWrapperClass = '.tab-bar',
-		tabClass		= '.tab',
-		tabClass    	= tabWrapperClass + ' ' +tabClass,
-		$tabs       	= $(tabClass); //for cloneRandomTab
-
-	$(d).on('ready', function() {
-		sortTabs();
-		bindEvents();
-		positionTabBar(0);
-		resizeTabBar();
-	});
-
-	function bindEvents() {
-		$(d).on('click', '.add-new', onAddNewClick);
-		$(d).on('click', '.close-all', onCloseAllClick);
-		$(d).on('click', '.close-all-but-active', onKeepActiveClick);
-		$(d).on('click', '.close-all-but-pinned', onKeepPinnedClick);
-		$(d).on('click', 'button.navigate-right', onNextClick);
-		$(d).on('click', 'button.navigate-left', onPrevClick);
-		//$(d).on('click', 'button.toggle-context-menu', onContextMenuClick);
-
-		//Tab clicks
-		$(d).on('click', '.tab-content', onTabContentClick);
-		$(d).on('click', '.close', onCloseClick);
-		$(d).on('click', '.pin', onPinClick);
-	}
-
+// js here
 
 	
 }(window, document, jQuery);
